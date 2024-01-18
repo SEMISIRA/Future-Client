70d885c922af6ed54379324c8acfb98c13c9d861
Dinhero21 <78858626+Dinhero21@users.noreply.github.com>
2024-01-18 10:28:18 -0300

    Better Transpilation

--- script/copy.sh
+++ script/copy.sh
@@ -4,12 +4,17 @@ SOURCE=$1
 
 if [ -z "$SOURCE" ]
 then
-  SOURCE="src/"
+  SOURCE="src"
 fi
 
 # length of "src/" = 4
 TARGET="${SOURCE:4}"
 
+if [ -z "$TARGET" ]
+then
+  TARGET="."
+fi
+
 TARGET="dist/$TARGET"
 
-rsync -rav --exclude="*.ts" $SOURCE $TARGET
+rsync -rav --exclude="*.ts" $SOURCE/ $TARGET/
--- script/watch/copy.sh
+++ script/watch/copy.sh
@@ -1,1 +1,3 @@
+./script/copy.sh
+
 inotifywait -rm -e CLOSE_WRITE --format "%w" src | stdbuf -o0 sed 's@/$@@' | xargs -n1 -I{} ./script/copy.sh {}
